d83d1792f278ffffde5e17909f3cd0ce9e07aaef
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2026-04-18 12:50:08 +0200

    test stats

--- stats.py
+++ stats.py
@@ -356,7 +356,6 @@ def _mermaid_illus_chart(rows_split: list) -> str:
 
     lines = [
         "```mermaid",
-        '%%{init: {"xychart": {"width": 2400, "useMaxWidth": false}}}%%',
         "xychart-beta",
         '    title "Illustrations par scène (% des mots)"',
         f"    x-axis [{', '.join(labels)}]",
